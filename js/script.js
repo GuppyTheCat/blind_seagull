07b25cb084ae5289b1d2014c360d42b5d22f0cba
GuppyTheCat <46162723+GuppyTheCat@users.noreply.github.com>
2019-08-12 16:25:19 +0200

    upload

--- js/script.js
+++ js/script.js
@@ -2,23 +2,18 @@ $(document).ready(function() {
     $(".discography-thumb").hover(
         function() {
             var item = $(this).children(".discography-popup");
-            //$(this).children(".discography-popup").css("display","flex");
             item.css("display", "flex");
-            item.animate({ opacity: 0.85 });
+            item.animate({ opacity: 0.85 }, { duration: 200 });
         },
         function() {
             var item = $(this).children(".discography-popup");
-            //$(this).children(".discography-popup").css("display","none");
-            item.animate({ opacity: 0 }, { duration: 100 }, function() { item.css("display", "none") });
+            item.animate({ opacity: 0 }, { duration: 200 }, function() { item.css("display", "none") });
         }
     );
-    $(".fa-vk").click(function() {
-        console.log("vk");
-    });
-    $(".fa-bandcamp").click(function() {
-        console.log("bandcamp");
-    });
-    $(".fa-soundcloud").click(function() {
-        console.log("soundcloud");
+
+    $("i").click(function() {
+    	//console.log($(this).attr("data-link"));
+    	window.open($(this).attr("data-link"));
+    	return false;
     });
 });
