3391f8b51b70867241f790aa3eed976addd4089d
GuppyTheCat <46162723+GuppyTheCat@users.noreply.github.com>
2019-08-14 17:18:51 +0200

    upload

--- js/script.js
+++ js/script.js
@@ -12,12 +12,31 @@ $(document).ready(function() {
     );
     $("i").click(function() {
         var parent = $(this).parent().parent();
-        if (parent.attr("id") == "discography-popup") {
+        if (parent.hasClass("discography-popup")) {
             if (parent.css("display") == "flex") {
                 window.open($(this).attr("data-link"));
             }
         } else {
             window.open($(this).attr("data-link"));
+        }
+    });
+
+    $.fn.isInViewport = function() {
+        var elementTop = $(this).offset().top;
+        var elementBottom = elementTop + $(this).outerHeight();
+
+        var viewportTop = $(window).scrollTop();
+        var viewportBottom = viewportTop + $(window).height();
+
+        return elementBottom > viewportTop && elementTop < viewportBottom;
+    };
+
+
+    $(window).scroll(function() {
+        if ($("#top-header").isInViewport()) {
+            $("#main-header").removeClass('scrolled');
+        } else {
+            $("#main-header").addClass('scrolled');
         }
     });
 });
